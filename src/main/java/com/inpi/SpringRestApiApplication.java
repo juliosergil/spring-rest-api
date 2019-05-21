784c4f34024cb6cd30394dae0119f711205abb55
Julio Lima <juliosergil@gmail.com>
2019-05-20 21:15:19 -0300

    Configurando o Aplication da API

--- src/main/java/com/inpi/SpringRestApiApplication.java
+++ src/main/java/com/inpi/SpringRestApiApplication.java
@@ -1,9 +1,24 @@
 package com.inpi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"com.inpi.model"})
+@ComponentScan(basePackages = {"com.*"})
+@EnableJpaRepositories(basePackages = {"com.inpi.repository"})
+@EnableTransactionManagement
+@EnableWebMvc
+@RestController
+@EnableAutoConfiguration
 public class SpringRestApiApplication {
 
 	public static void main(String[] args) {
